5c67fcc1b50516fbd8666fcfcf2b7ef58ba29b53
nabilk11 <khan.nabiln11@gmail.com>
2022-02-12 01:43:46 -0500

    fixed spacing between kawaiis and added labels

--- src/components/ReactKawaii/ReactKawaii.jsx
+++ src/components/ReactKawaii/ReactKawaii.jsx
@@ -5,14 +5,17 @@ export default function ReactKawaii({ got, pokemon }) {
     return (
         <div className="section">
         <h2 className='section-head'>React Kawaiis</h2>
+        <div className="kawaii-parent">
         <div className="got-kawaii">
-  
+        Game of Thrones Kawaii
         {got.sentence ? <Cat size={200} mood="lovestruck" color="#DC143C" /> 
          : <Cat size={200} mood="sad" color="#0000FF" /> }
         </div>
         <div className="pokemon-kawaii">
+        Pokemon Kawaii
         {pokemon.length >0 ? <Chocolate size={200} mood="lovestruck" color="#DC143C" />
         : <Chocolate size={200} mood="sad" color="#0000FF" />}
+        </div>
         </div>
       </div>
   )
