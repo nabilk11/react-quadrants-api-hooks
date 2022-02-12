c1b9f64da0104d624fa79359105f979694b30e33
nabilk11 <khan.nabiln11@gmail.com>
2022-02-12 01:30:36 -0500

    added additional fields to GameOfThrones.jsx

--- src/components/GameOfThrones/GameOfThrones.jsx
+++ src/components/GameOfThrones/GameOfThrones.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 export default function GameOfThrones({ got }) {
     return (
         <div className="section">
-
-        <div>
-          <h2>{got.sentence}</h2>
-          {got.character &&  <h2>By: {got.character.name}</h2> }
-
+        <h1>Game of Thrones<br />
+      <small>Random Quotes</small></h1>
+           <div className='got-quote'>
+             <h2>{got.sentence}</h2>        
+          {got.character &&  <h3>By: {got.character.name}</h3> }
+          {got.character &&  <h4>{got.character.house.name}</h4> }
         </div>
         <div>
 
